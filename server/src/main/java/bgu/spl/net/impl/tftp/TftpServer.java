757488d2cf66a810bdf772e38b18c2ae031b5b88
tamir <taamirhd@gmail.com>
2024-03-05 18:17:15 +0200

    starting

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -1,5 +1,16 @@
 package bgu.spl.net.impl.tftp;
+import bgu.spl.net.api.BidiMessagingProtocol;
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.srv.Server;
 
 public class TftpServer {
-    //TODO: Implement this
-}
+
+    public static void main(String[] args) {
+        Server.threadPerClient(7777,
+         () -> new TftpProtocol(),
+          TftpEncoderDecoder::new).serve();
+
+
+    }
+    }   
--- server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -1,5 +1,6 @@
 package bgu.spl.net.srv;
 
+import bgu.spl.net.api.BidiMessagingProtocol;
 import bgu.spl.net.api.MessageEncoderDecoder;
 import bgu.spl.net.api.MessagingProtocol;
 import java.io.BufferedInputStream;
@@ -9,14 +10,14 @@ import java.net.Socket;
 
 public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
 
-    private final MessagingProtocol<T> protocol;
+    private final BidiMessagingProtocol<T> protocol;
     private final MessageEncoderDecoder<T> encdec;
     private final Socket sock;
     private BufferedInputStream in;
     private BufferedOutputStream out;
     private volatile boolean connected = true;
 
-    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, MessagingProtocol<T> protocol) {
+    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, BidiMessagingProtocol<T> protocol) {
         this.sock = sock;
         this.encdec = reader;
         this.protocol = protocol;
@@ -33,13 +34,13 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
             while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
                 T nextMessage = encdec.decodeNextByte((byte) read);
                 if (nextMessage != null) {
-                    T response = protocol.process(nextMessage);
-                    if (response != null) {
-                        out.write(encdec.encode(response));
-                        out.flush();
+                    protocol.process(nextMessage);
+                    // if (response != null) {
+                    //     out.write(encdec.encode(response));
+                    //     out.flush();
                     }
                 }
-            }
+            
 
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -54,7 +55,14 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
     }
 
     @Override
-    public void send(T msg) {
-        //IMPLEMENT IF NEEDED
+ public void send(T msg) {
+    try{
+        if (msg != null) {
+            out.write(encdec.encode(msg));
+            out.flush();    }
+    }catch (IOException ex) {
+        ex.printStackTrace();
+    }
+    
     }
 }
